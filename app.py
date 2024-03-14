8b624c867a9787fa19ea2f064cd9df0c98282fdb
harry-xdev <harry.xdev@gmail.com>
2024-03-14 21:22:42 +0700

    add console button click

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # return render_template("index.html")
-    return "Hello, World!"
+    return render_template("index.html")
+    # return "Hello, World!"
 
 @app.route("/button-click", methods=["POST"])
 def button_click():
